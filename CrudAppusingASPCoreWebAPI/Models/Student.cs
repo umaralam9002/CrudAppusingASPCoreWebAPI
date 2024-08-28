848113163e44f74fb0315a233303351579bf2b3c
umaralam9002 <umaralam9002@gmail.com>
2024-08-28 14:21:28 +0500

    Initial commit

---
+++ CrudAppusingASPCoreWebAPI/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+
+namespace CrudAppusingASPCoreWebAPI.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        [Required]
+        public string studentName { get; set; }
+        [Required]
+        public string studentGender { get; set; }
+        [Required]
+        public int age { get; set; }
+        [Required]
+        public int Class { get; set; }
+        [Required]
+        public string fatherName { get; set; }
+    }
+}
+
+
